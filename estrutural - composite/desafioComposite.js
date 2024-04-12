9b7b632df53c1e03a8d20eec757b9c43c93c02e3
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2024-04-12 11:10:13 -0300

    Add files via upload

---
+++ estrutural - composite/desafioComposite.js
@@ -0,0 +1,8 @@
+//criar curso dsm
+//incluir turmas 1, 2, 3, 4, 5, 6
+//cada turma add 5 alunos
+//info ra(4), nome completo(20), nota(p1, p2, p3, p4, media final), status
+
+//adicionar "interface"
+//dados do aluno: nome, email, curso e semestre
+//dados academico: ra, nome, relação , relação de alunos,notas
