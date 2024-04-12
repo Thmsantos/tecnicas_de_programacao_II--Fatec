db3654505fb1c7dc57f4631f28bfac895aea3ace
Thiago Santos <91476713+Thmsantos@users.noreply.github.com>
2024-04-12 08:50:41 -0300

    Delete Aula Dois - Padrões de projetos/FactoryMethod - arquivos directory

--- Aula Dois - Padrões de projetos/FactoryMethod - arquivos/FactoryMethod/src/Carro.java
+++
@@ -1,5 +0,0 @@
-public class Carro extends Veiculo {
-    public Veiculo(String modelo){
-        super(modelo)
-    };
-}
--- Aula Dois - Padrões de projetos/FactoryMethod - arquivos/desafio catraca/arquivos/src/Funcionario.java
+++
@@ -1,9 +0,0 @@
-public class Funcionario extends Pessoa {
-    public Funcionario(String tipo) {
-        super(tipo);
-    }
-
-    public String MostrarNome() {
-        return "Thiago";
-    }
-}
